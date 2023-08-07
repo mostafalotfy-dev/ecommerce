a5e9dc008b3706ed1f6480c8a212695f0be4e4a9
mostafa lotfy <mostafalotfy285@gmail.com>
2023-08-07 13:10:22 +0300

    add status to searchable fields

--- app/Repositories/BranchRepository.php
+++ app/Repositories/BranchRepository.php
@@ -9,14 +9,16 @@ class BranchRepository extends Repository
 		"name_en",
 		"name_ar",
 		"description_en",
-
 		"description_ar",
+        "status",
+        "is_cod",
 	];
 	static $rules = [
 		"name_en"=>'required|max:255',
 		"name_ar"=>"required|max:255",
 		"description_en"=>"required|string",
 		"description_ar"=>"required|string",
+        "status"=>"nullable|in:0,1"
 	];
 	public function tableName():string
 	{
